5b01cc609286b14bc643fc4cdb16c14ac8c842e9
stinai <134688445+stinai@users.noreply.github.com>
2024-01-22 13:56:12 +0800

    solved

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Commonly used data types DO NOT include:",
+        answer: "3. alerts",
+        choices: [
+            "1. strings",
+            "2. booleans",
+            "3. alerts",
+            "4. numbers"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if / else statement is enclosed within _____.",
+        answer: "3. parantheses",
+        choices: [
+            "1. quotes",
+            "2. curly brackets",
+            "3. parantheses",
+            "4. square brackets"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store _____.",
+        answer: "4. all of the above",
+        choices: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answer: "3. quotes",
+        choices: [
+            "1. commas",
+            "2. curly brackets",
+            "3. quotes",
+            "4. parantheses"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "4. console.log",
+        choices: [
+            "1. JavaScript",
+            "2. termin / bash",
+            "3. for loops",
+            "4. console.log"
+        ]
+    },
+];
